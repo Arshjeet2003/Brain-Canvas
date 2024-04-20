9cb10bbc4a528ed49a4a81116f4819c601eca4db
Arshjeet2003 <arshjeetsingh123@gmail.com>
2024-04-20 17:17:58 +0530

    added boardlist.js file to show boards charts

---
+++ client/src/components/BoardList.js
@@ -0,0 +1,160 @@
+import React, { useContext, useEffect, useState } from "react";
+import { Link, useNavigate, useParams, useLocation } from "react-router-dom";
+import "../css/boardlist.css";
+import LinkContext from "../context/links/LinkContext.js";
+import BoardContext from "../context/boards/BoardContext.js";
+import firstImg from "../images/1.svg";
+import secondImg from "../images/2.svg";
+import fourthImg from "../images/4.svg";
+import eight from "../images/8.svg";
+import nine from "../images/9.svg";
+import ten from "../images/10.svg";
+
+const img = [firstImg, secondImg, fourthImg, eight, nine, ten];
+
+const BoardList = (props) => {
+
+  const { link, updateLink, createLink, joinLinkById, createNewBoard } =
+    useContext(LinkContext);
+
+  const { linkId } = useParams();
+  const navigate = useNavigate();
+
+  const [boards, setBoards] = useState([]);
+  const [newBoardTitle, setNewBoardTitle] = useState("");
+  const [newBoardDescription, setNewBoardDescription] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await joinLinkById(linkId);
+        setBoards(response.data.boards);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, [linkId, newBoardTitle, newBoardDescription]);
+
+  const handleNewBoardCreation = async (e) => {
+    e.preventDefault();
+    e.preventDefault();
+    const title = e.target.elements.title.value;
+    const description = e.target.elements.description.value;
+    // await createNewBoard(linkId, title, description);
+    const response = await createNewBoard(linkId, title, description);
+    console.log(response.data);
+    setNewBoardTitle(title);
+    setNewBoardDescription(description); 
+    // navigate(`/${linkId}/board/${boardId}`);
+  };
+
+  return (
+    <>
+    <div>
+        Create new idea board :
+        <form onSubmit={handleNewBoardCreation}>
+          <label htmlFor="title">title</label>
+          <input type="text" id="title" name="title" />
+          <label htmlFor="description">description</label>
+          <input type="text" id="description" name="description" />
+          <button type="submit">Add Board</button>
+        </form>
+      </div>
+    <div className="bodyOfBoardList">
+      <div className="row">
+        <div className="col-6 mt-3 mx-4">
+          <h1 className="heading">Helping the world</h1>
+          <h5 className="heading2">
+            More than 180,000 organizations, including Nike, Ikea, Deloitte,
+            WPP, and Cisco, have adopted Miro to help innovate the way they
+            innovate, with the goal of improving the speed and quality of their
+            work.
+          </h5>
+        </div>
+      </div>
+
+      <div className="row mt-3 mx-4 heading">
+        <div className="col-4">
+          <strong>
+            <h3>Team Name : </h3>
+          </strong>
+          <span className="heading2">{boards.name}</span>
+        </div>
+        <div className="col-4">
+          <strong>
+            <h3>Team Code : </h3>
+          </strong>
+          <span className="heading2">{boards.code}</span>
+        </div>
+      </div>
+      <div className="container main">
+        {boards.map((board, index) => (
+          <div className="item">
+            <Link
+              to={`/${linkId}/board/${board._id}`}
+              key={board.id}
+              className="card my-5"
+              style={{
+                textDecoration: "none",
+                border: "none",
+                marginBottom:
+                  index < boards.length - 1
+                    ? "10px !important"
+                    : "0 !important",
+              }}
+            >
+              <div className="section_our_solution">
+                <div className="row">
+                  <div className="col">
+                    <div className="our_solution_category">
+                      <div className="solution_cards_box">
+                        <div className="solution_card">
+                          <div className="hover_color_bubble" />
+                          <div className="row">
+                            <div className="col-7">
+                              <div className="solu_title">
+                                <h3>{board.title}</h3>
+                              </div>
+                              <div className="solu_description">
+                                <p>{board.description}</p>
+                              </div>
+                              <div className="row">
+                                <div className="col solu_description ">
+                                  <button
+                                    type="button"
+                                    className="read_more_btn"
+                                  >
+                                    Read More
+                                  </button>
+                                </div>
+                              </div>
+                            </div>
+                            <div className="col-5">
+                            <div className="so_top_icon">
+                                {
+                                  <img
+                                    src={img[index % img.length]}
+                                    alt="SVG Image"
+                                    style={{ height: "40vh", width: "40vh" }}
+                                  />
+                                }
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+    </>
+  );
+};
+
+export default BoardList;
