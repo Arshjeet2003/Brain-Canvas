7974d900df69427f2a89858f1ca6593d35bda182
Arshjeet2003 <arshjeetsingh123@gmail.com>
2024-04-21 06:42:13 +0530

    updated board.js

--- client/src/components/Board.js
+++ client/src/components/Board.js
@@ -18,6 +18,8 @@ const Board = () => {
   const { linkId, boardId } = useParams();
 
   const [charts, setCharts] = useState([]);
+  const [newChartTitle, setNewChartTitle] = useState("");
+  const [newChartDescription, setNewChartDescription] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -29,81 +31,103 @@ const Board = () => {
       }
     };
     fetchData();
-  }, [boardId, linkId])
+  }, [boardId, linkId, newChartTitle, newChartDescription])
 
 const handleNewChartCreation = async (e) => {
   e.preventDefault();
   const title = await e.target.elements.title.value;
   const description = await e.target.elements.description.value;
   const chartId = await createNewChart(linkId, boardId, title, description);
-  // const link = await joinLinkById(linkId);
-  console.log(chartId);
-  navigate(`/${linkId}/board/${boardId}/charts/${chartId}`);
+  setNewChartTitle(title)
+  setNewChartDescription(description)
+  // navigate(`/${linkId}/board/${boardId}/charts/${chartId}`);
 };
 
   return (
     <>
-    <div>
-        Create new idea Chart :
-        <form onSubmit={handleNewChartCreation}>
-          <label htmlFor="title">title</label>
-          <input type="text" id="title" name="title" />
-          <label htmlFor="description">description</label>
-          <input type="text" id="description" name="description" />
-          <button type="submit">Add Chart</button>
-        </form>
-      </div>
-    <div className="bodyOfBoard">
-      {/* {board.title}
+      <div className="bodyOfBoard container-fluid">
+        {/* {board.title}
       {board.teamCode}
       {board.userName} */}
+        
+        <div className="row">
+          <div className="col-6 mt-3 mx-4">
+            <h1 className="heading">Helping the world</h1>
+            <h5 className="heading2 mt-5">
+              More than 180,000 organizations, including Nike, Ikea, Deloitte,
+              WPP, and Cisco, have adopted Miro to help innovate the way they
+              innovate, with the goal of improving the speed and quality of
+              their work.
+            </h5>
+          </div>
+          <div className="col-4">
+            <div className="row  titleInputBoardList mt-1">
+              <div className="biggestTitle ">Create new idea Chart </div>
+              <form onSubmit={handleNewChartCreation}>
+                <input
+                  type="text"
+                  id="title"
+                  name="title"
+                  placeholder="title"
+                  className="mt-3 inputBoardList"
+                />
 
-      <div className="row">
-        <div className="col-6 mt-3 mx-4">
-          <h1 className="heading">Helping the world</h1>
-          <h5 className="heading2">
-            More than 180,000 organizations, including Nike, Ikea, Deloitte,
-            WPP, and Cisco, have adopted Miro to help innovate the way they
-            innovate, with the goal of improving the speed and quality of their
-            work.
-          </h5>
-        </div>
-      </div>
+                <textarea
+                  type="text"
+                  id="description"
+                  name="description"
+                  placeholder="description"
+                  className="mt-3 inputBoardList textareaBl"
+                />
 
-      <div className="chart-list container ">
-        {charts.map((chart ,index) => (
-          <div className="item" key={chart.id}>
-            <Link
-              to={`/${linkId}/board/${boardId}/charts/${chart._id}`}
-              key={chart.id}
-              // className="card my-3 mx-3"
-              style={{ textDecoration: "none" }}
-            >
-              <div className="first">
                 <div className="row">
-                  <div className="col">
-                    <div className="second">
-                      <div className="third">
-                        <div className="forth">
-                          <div className="fifth" />
-                          <div className="row">
-                            <div className="col">
-                              <div className="sixth">
-                                <h3>{chart.title}</h3>
-                              </div>
-                            </div>
+                  <div className="col-4"></div>
+                  <div className="col-8">
+                    <div className="biggestButton">
+                      <button type="submit">Add Chart</button>
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
 
-                            {/* <div className="seventh">
+        <div className="chart-list container ">
+          {charts.map((chart, index) => (
+            <div className="item" key={chart.id}>
+              <Link
+                to={`/${linkId}/board/${boardId}/charts/${chart._id}`}
+                key={chart.id}
+                // className="card my-3 mx-3"
+                style={{ textDecoration: "none" }}
+              >
+                <div className="first">
+                  <div className="row">
+                    <div className="col">
+                      <div className="second">
+                        <div className="third">
+                          <div className="forth">
+                            <div className="fifth" />
+                            <div className="row">
+                              <div className="col">
+                                <div className="sixth">
+                                  <h3>{chart.title}</h3>
+                                </div>
+                              </div>
+
+                              {/* <div className="seventh">
                                 <p>{chart.description}</p>
                               </div> */}
-                            <div className="row">
-                              <div className="col extra">
-                                <div className="so_top_icon">
-                                <img
+                              <div className="row">
+                                <div className="col extra">
+                                  <div className="so_top_icon borderborder">
+                                    <img
                                       src={img[index % img.length]}
                                       alt="SVG Image"
                                       style={{ height: "40vh", width: "40vh" }}
                                     />
+                                  </div>
                                 </div>
                               </div>
                             </div>
@@ -113,13 +137,12 @@ const handleNewChartCreation = async (e) => {
                     </div>
                   </div>
                 </div>
-              </div>
-              {/* </div> */}
-            </Link>
-          </div>
-        ))}
+                {/* </div> */}
+              </Link>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
     </>
   );
 };
